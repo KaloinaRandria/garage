34b0d48b67ea6ebaf0bafebf3420b239a9149b0d
Kaloina RANDRIAMBOLOLONA <143845374+KaloinaRandria@users.noreply.github.com>
2024-07-15 16:17:33 +0300

    Create script.sql

---
+++ script.sql
@@ -0,0 +1,31 @@
+create table type (
+    id int primary key auto_increment,
+    intitule varchar(100)
+);
+
+create table client (
+    id int primary key auto_increment,
+    numero varchar(20) unique ,
+    id_type int references type(id)
+);
+
+create table service (
+    id int primary key auto_increment,
+    intitule varchar(100),
+    duree time not null,
+    prix double
+);
+
+create table slot (
+    id int primary key auto_increment,
+    intitule varchar(100)
+);
+
+create table reservation (
+    id int primary key auto_increment,
+    date_heure_debut datetime,
+    date_heure_fin datetime,
+    id_service int references service(id),
+    id_slot int references slot(id),
+    id_client int references client(id)
+);
