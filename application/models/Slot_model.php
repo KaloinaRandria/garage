b767ca889d075705b3493127f2c0bb5759e42cbe
Kaloina RANDRIAMBOLOLONA <143845374+KaloinaRandria@users.noreply.github.com>
2024-07-15 22:17:46 +0300

    fonction chercher Date Fin

--- application/models/Slot_model.php
+++ application/models/Slot_model.php
@@ -20,6 +20,26 @@
 			$query = $this->db->get('slot');
 			return $query->row();
 		}
+
+		function checkSlotLibre($dateDebut , $dateFin) {
+			
+		}
+
+		function searchDateFin($dateDebut ,$idService) {
+			$this->load->model('Service_model');
+			$service = $this->service_model->getServiceById($idService);
+			$date = new DateTime($dateDebut);
+			$interval = new DateInterval('PT'.$service->duree);
+			$dateFin = $date->add($interval);
+			
+			$temp = $date->format('H:i:s');
+			if (($temp > new DateTime('18:00:00') && $temp < new DateTime('23:59:00')) ||
+				($temp > new DateTime('00:00:00') && $temp < new DateTime('08:00:00'))) {
+				$dateFin = $dateFin->add(new DateInterval('14:00:00')); 
+			}
+
+			return $dateFin;
+		}
 	}
 
 ?>
