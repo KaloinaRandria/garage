52362f5386dc02ed3902b9ddeb964053a24729de
Kaloina RANDRIAMBOLOLONA <143845374+KaloinaRandria@users.noreply.github.com>
2024-07-16 00:17:56 +0300

    kely sisa

--- application/models/Slot_model.php
+++ application/models/Slot_model.php
@@ -25,13 +25,13 @@ class Slot_model extends CI_Model
 		return $query->row();
 	}
 
-	function checkSlotLibre($dateDebut ,$idService)
+	function checkSlotLibre($dateDebut, $idService)
 	{
-		$this->load->model('slot_model');
-		$slots = $this->slot_model->getSlots();
-		
-		foreach($slots  as $slot) {
-			$query = $this->db->query($this->dynamicQuery($slot['id'], $dateDebut,$idService));
+
+		$slots = $this->getSlots();
+
+		foreach ($slots  as $slot) {
+			$query = $this->db->query($this->dynamicQuery($slot['id'], $dateDebut, $idService));
 			if ($query->num_rows() == 1) {
 				return $query->result_array();
 			}
@@ -41,32 +41,48 @@ class Slot_model extends CI_Model
 
 	function searchDateFin($dateDebut, $idService)
 	{
-		$this->load->model('Service_model');
+		$this->load->model('service_model'); // Ensure Service_model is loaded
 		$service = $this->service_model->getServiceById($idService);
+
 		$date = new DateTime($dateDebut);
-		$interval = new DateInterval('PT' . $service['duree']);
+		
+		// Assume $service->duree is in format "H:i"
+		list($hours, $minutes) = explode(':', $service->duree);
+		$interval = new DateInterval('PT' . $hours . 'H' . $minutes . 'M');
+
 		$dateFin = $date->add($interval);
 
 		$temp = $date->format('H:i:s');
-		if (($temp > new DateTime('18:00:00') && $temp < new DateTime('23:59:00')) ||
-			($temp > new DateTime('00:00:00') && $temp < new DateTime('08:00:00'))
+		$evening_start = new DateTime('18:00:00');
+		$night_end = new DateTime('23:59:59');
+		$morning_start = new DateTime('00:00:00');
+		$morning_end = new DateTime('08:00:00');
+
+		if (($temp > $evening_start->format('H:i:s') && $temp < $night_end->format('H:i:s')) ||
+			($temp > $morning_start->format('H:i:s') && $temp < $morning_end->format('H:i:s'))
 		) {
-			$dateFin = $dateFin->add(new DateInterval('14:00:00'));
+			$dateFin = $dateFin->add(new DateInterval('PT14H'));
 		}
 
 		return $dateFin;
 	}
 
-	function dynamicQuery($idSlots , $dateDebut, $idService)
-	{
-			$sql = " select id_slot from reservation where 1=1 ";
-			$this->load->model('Reservation_model');
-			$reservations = $this->reservation_model->getReservationBySlot($idSlots);
-			$dateFin = $this->searchDateFin($dateDebut,$idService);
-			foreach($reservations as $reservation) {
-				$sql = $sql . " and (" . $dateDebut . " not between " . $reservation['date_heure_debut'] . " and " . $reservation['date_heure_fin'] . ") and (" . $dateFin . " not between " . $reservation['date_heure_debut'] . " and " . $reservation['date_heure_fin'] . ")";
-			}
-			$sql = $sql . " limit 1 ";
-			return $sql;
-	}
+	function dynamicQuery($idSlots, $dateDebut, $idService)
+    {
+        $this->load->model('reservation_model');
+        $sql = "SELECT id_slot FROM reservation WHERE 1=1 AND DATE(date_heure_debut) = '".$dateDebut->format('y-m-d')."'";
+        $reservations = $this->reservation_model->getReservationBySlots($idSlots);
+        $dateFin = $this->searchDateFin($dateDebut, $idService);
+        
+        foreach ($reservations as $reservation) {
+            $sql .= " AND ('" . $dateDebut . "' NOT BETWEEN '" . $reservation->date_heure_debut . "' AND '" . $reservation->date_heure_fin . "')";
+            $sql .= " AND ('" . $dateFin->format('Y-m-d H:i:s') . "' NOT BETWEEN '" . $reservation->date_heure_debut . "' AND '" . $reservation->date_heure_fin . "')";
+			$sql .= " AND ('" . $reservation->date_heure_debut . "' NOT BETWEEN '" . $dateDebut . "' AND '" . $dateFin->format('Y-m-d H:i:s') . "')";
+			$sql .= " AND ('" . $reservation->date_heure_fin . "' NOT BETWEEN '" . $dateDebut . "' AND '" . $dateFin->format('Y-m-d H:i:s') . "')";
+
+        }
+        
+        $sql .= " LIMIT 1";
+        return $sql;
+    }
 }
