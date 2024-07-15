cea5da7f0a83e5f1ee49beaea866728a7b5e0c3d
Kaloina RANDRIAMBOLOLONA <143845374+KaloinaRandria@users.noreply.github.com>
2024-07-15 18:35:23 +0300

    Update Auth.php

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -1,12 +1,32 @@
 <?php
-	class Auth extends CI_Controller {
-		function __construct()
-		{
-			parent::__construct();
-		}
 
-		function login() {
-			
+/**
+ * @property CI_Input $input
+ * @property CI_Session $session
+ * @property Client_model $client_model
+ */
+class Auth extends CI_Controller
+{
+	function __construct()
+	{
+		parent::__construct();
+	}
+
+	function login()
+	{
+	}
+
+	function checkLogin()
+	{
+		$numero = $this->input->post('numero');
+		$type = $this->input->post('type');
+		if ($this->client_model->checkLogin($numero, $type)) {
+			$this->session->set_userdata('numero',$numero);
+			$this->session->set_userdata('type',$type);
+			redirect('front_office/pages/accueil');
+		} else {
+			$this->session->set_flashdata('error','Immatriculation Invalide ou Type Invalide');
+			redirect('front_office/login/login');	
 		}
 	}
-?>
+}
