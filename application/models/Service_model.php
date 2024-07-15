ff19aff186e5c94479adce35079753a4367b0d70
Kaloina RANDRIAMBOLOLONA <143845374+KaloinaRandria@users.noreply.github.com>
2024-07-15 16:23:34 +0300

    updateService(data , id)

--- application/models/Service_model.php
+++ application/models/Service_model.php
@@ -19,5 +19,19 @@
 			$query = $this->db->get('garage');
 			return $query->row();
 		}
+
+		function updateService ($data, $id) {
+			$this->db->where('id',$id);
+			$this->db->update('garage',$data);
+			if ($this->db->affected_rows() >= 0) {
+				return true;
+			} else {
+				return false;
+			}
+		}
+
+		function deleteService($id) {
+			
+		}
 	}
 ?>
